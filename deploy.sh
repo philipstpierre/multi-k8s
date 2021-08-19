54b19f5f353e591b6707d6af963a6421661dc077
Philip St-Pierre <philip.stpierre@gmail.com>
2021-08-18 22:25:33 -0400

    Fixed typo in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t stpiep/multi-client:latest -t stpiep/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stpiep/multi-server:latest -t stpiep/mutli-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stpiep/multi-worker:latest -t stpiep/mutli-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t stpiep/multi-server:latest -t stpiep/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t stpiep/multi-worker:latest -t stpiep/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push stpiep/multi-client:latest
 docker push stpiep/multi-server:latest
